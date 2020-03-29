cee46c3e590bf7ae3bc9230905262f796fcd2776
Moran-KC <62290067+Moran-KC@users.noreply.github.com>
2020-03-29 23:15:55 +0300

    Add files via upload

---
+++ Matala1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Matala1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //הגדרת משתנים
+            int num;
+            int mul;
+            //קליטת גודל מערך
+            Console.WriteLine("Enter num of elements");
+            num=Convert.ToInt32(Console.ReadLine());
+            //הגדרת המערך לאחר הקליטה של הגודל-בכדי שתעבוד ההרצה
+            int []arr = new int[num];
+            //קליטת מס למכפלה 
+            Console.WriteLine("enter num of mul");
+            mul = Convert.ToInt32(Console.ReadLine());
+            //מילוי המערך לפי דרישה
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * mul;
+                Console.Write(arr[i] + ", ");
+
+            }
+            //הדפסת מס התאים , מס המכפלה , וכל תאי המערך
+            Console.WriteLine($"Num of elements: {num}, num to mul:{mul}");
+        }
+    }
+}
