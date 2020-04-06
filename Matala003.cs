6249049f0612229ae2669a871e0447da8f8f7973
Moran-KC <62290067+Moran-KC@users.noreply.github.com>
2020-04-07 00:51:34 +0300

    Add files via upload

---
+++ Matala003.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Matala003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] matrix = new string[3][];
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix.Length; col++)
+                {
+                    if (row == col && row + col == matrix.Length - 1)
+                    {
+
+                        Console.Write(" Center, ");
+                    }
+                    else if (row == col)
+                    {
+
+                        Console.Write(" main, ");
+                    }
+                    else if (row + col == matrix.Length - 1)
+                    {
+
+                        Console.Write(" sub, ");
+                                            }
+                    else if (row < col)
+                    {
+
+                        Console.Write(" up ,");
+                    }
+                    else
+                    {
+                        Console.Write(" down ,");
+                    }  
+                }
+                Console.WriteLine();
+
+            }
+          }
+    }
+}
