6274ad4d0d664070dd3bb34e2f3715d2e5c88f82
Moran-KC <62290067+Moran-KC@users.noreply.github.com>
2020-04-19 22:57:48 +0300

    Add files via upload
    Matala3B

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassTask3b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int x = 1;
+            do
+            {
+                Console.WriteLine("enter a number between 3 to 9");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            while (num < 3 || num > 9);
+            int[][] matrix= new int[num][];
+            for (int row = 0; row < matrix.Length; row++, x++)
+            {
+                matrix[row] = new int[row + 1];
+
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    matrix[row][col] = x;
+                }
+            }
+
+        }
+    }
+}
